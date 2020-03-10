23ecf8ed220c00034ac36774d99d3684a64cd990
haoyuexing <haoyuexing@gmail.com>
2020-03-10 15:19:42 +0800

    编辑收货地址

--- page/address_list_page.py
+++ page/address_list_page.py
@@ -9,8 +9,13 @@ class AddressListPage(BaseAction):
 
     name_and_phone_label = By.ID, "com.yunmall.lc:id/receipt_name"
 
+    default_label = By.ID, "com.yunmall.lc:id/address_is_default"
+
     def click_add_address(self):
         self.click(self.add_address_button)
 
     def get_first_name_and_phone_text(self):
         return self.get_text(self.name_and_phone_label)
+
+    def click_default(self):
+        self.click(self.default_label)
--- scripts/test_address.py
+++ scripts/test_address.py
@@ -59,3 +59,39 @@ class TestAddress:
         else:
             assert self.page.edit_address.is_toast_exist(toast)
 
+    def test_edit_address(self):
+
+        # 首页 - 点击关闭
+        self.page.home.click_close()
+        # 首页 - 如果没有登录就去登录
+        self.page.home.login_if_not(self.page)
+        # 首页 - 点击我
+        self.page.home.click_me()
+        # 我 - 点击设置
+        self.page.me.click_setting()
+        # 设置 - 点击地址管理
+        self.page.setting.click_address_list()
+        # 地址列表 - 点击默认标记
+        self.page.address_list.click_default()
+
+        # 编辑地址 - 输入收件人
+        self.page.edit_address.input_name("xiaoqiang")
+        # 编辑地址 - 输入手机号
+        self.page.edit_address.input_phone("17777777777")
+        # 编辑地址 - 输入详细地址
+        self.page.edit_address.input_detail("三单元 504")
+        # 编辑地址 - 输入邮编
+        self.page.edit_address.input_post_code("200000")
+
+        # 编辑地址 - 点击选择区域
+        self.page.edit_address.click_region()
+        # 选择区域 - 开始选择
+        self.page.region.click_region()
+
+        # 编辑地址 - 保存
+        self.page.edit_address.click_save()
+
+        # 断言 是否有 保存成功的toast
+        assert self.page.address_list.is_toast_exist("保存成功")
+        # 断言 列表的第一个值 是否和刚刚输入的一样
+        assert self.page.address_list.get_first_name_and_phone_text() == "%s  %s" % ("xiaoqiang", "17777777777")
